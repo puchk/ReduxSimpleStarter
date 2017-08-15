d2555a30e4d6036b58d37abe9e54887cbd5460f0
puchk <kpuchalski@gmail.com>
2017-08-15 00:59:16 -0700

    Returns data from youtube api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDiNzHZAb_mzlUMLpJEWPwOBK9fWjRDU7c';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+			this.state = { videos: [] };
+
+			YTSearch({key: API_KEY, term: 'climbing'}, (videos) => {
+				this.setState({ videos });
+			});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
