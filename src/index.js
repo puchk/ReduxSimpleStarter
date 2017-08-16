71a2c86ae9e63447be719ba27b0c2fe0efb4d2d4
puchk <kpuchalski@gmail.com>
2017-08-16 13:03:30 -0700

    display thumbnail and title, embed main video

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDiNzHZAb_mzlUMLpJEWPwOBK9fWjRDU7c';
 
@@ -22,6 +24,8 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
