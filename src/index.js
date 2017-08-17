3b144a88986d5e8fe54aa92e3bb16fc0415caa7a
puchk <kpuchalski@gmail.com>
2017-08-16 23:31:12 -0700

    Click to change video, working search bar

--- src/index.js
+++ src/index.js
@@ -14,18 +14,30 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-			this.state = { videos: [] };
+			this.state = { 
+				videos: [],
+				selectedVideo: null
+			};
 
-			YTSearch({key: API_KEY, term: 'climbing'}, (videos) => {
-				this.setState({ videos });
+		this.videoSearch('climbing');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
 			});
+		});		
 	}
 	render() {
 		return (
 			<div>
-				<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
 			</div>
 		);
 	}
